644c5c31082b9586912ab6583ff7a840669b9fba
Fergoth <fergothew@gmail.com>
2019-04-09 14:39:29 +0300

    initial commit

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,35 @@
+import sys
+from collections import Counter
+import re
 
 
 def load_data(filepath):
-    pass
+    try:
+        with open(filepath, encoding='utf-8') as file:
+            text = file.read()
+            return text
+    except FileNotFoundError:
+        print('Файл не найден')
 
 
 def get_most_frequent_words(text):
-    pass
+    words = re.findall(r'\w+', text)
+    return Counter(words).most_common(10)
+
+
+def print_counter(count_object):
+    for word, freq in count_object:
+        print('слово "{}" встречается {} раз'.format(word, freq))
+
 
 
 if __name__ == '__main__':
-    pass
+    try:
+        path = sys.argv[1]
+    except IndexError:
+        print('Требуется путь к файлу как аргумент')
+    else:
+        text_from_file = load_data(path)
+        if text_from_file:
+            words_counter = get_most_frequent_words(text_from_file)
+            print_counter(words_counter)
