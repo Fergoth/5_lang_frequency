7827fa9681929c5897f4947d6e64ab02796dec8f
Fergoth <fergothew@gmail.com>
2019-04-10 07:33:21 +0300

    refactoring

--- lang_frequency.py
+++ lang_frequency.py
@@ -4,12 +4,10 @@ import re
 
 
 def load_data(filepath):
-    try:
-        with open(filepath, encoding='utf-8') as file:
-            text = file.read()
-            return text
-    except FileNotFoundError:
-        print('Файл не найден')
+    with open(filepath, encoding='utf-8') as file:
+        text = file.read()
+        return text
+
 
 
 def get_most_frequent_words(text, n_first_words=10):
@@ -26,9 +24,10 @@ if __name__ == '__main__':
     try:
         path = sys.argv[1]
     except IndexError:
-        print('Требуется путь к файлу как аргумент')
-    else:
-        text_from_file = load_data(path)
-        if text_from_file:
-            words_counter = get_most_frequent_words(text_from_file)
-            print_counter(words_counter)
+        sys.exit('Требуется путь к файлу как аргумент')
+    except FileNotFoundError:
+        sys.exit('Файл не найден')
+    text_from_file = load_data(path)
+    if text_from_file:
+        words_counter = get_most_frequent_words(text_from_file)
+        print_counter(words_counter)
